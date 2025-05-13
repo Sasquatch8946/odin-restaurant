68e7ab6c15b3afe9d1745e93a246b31bd961b421
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-05-13 15:25:25 -0500

    Add prices to the menu.

--- src/loadMenu.js
+++ src/loadMenu.js
@@ -1,17 +1,20 @@
 import pancakes from "./pancakes.jpeg";
 import bacon from "./bacon.jpeg";
 
-function createMenuItem(name, description, image) {
+function createMenuItem(name, description, image, price) {
     const menuItem1 = document.createElement("div");
     menuItem1.classList.add("menu-item");
     const title = document.createElement("div");
     title.innerText = name;
     const itemDescription = document.createElement("div");
     itemDescription.innerText = description;
+    const itemPrice = document.createElement("div");
+    itemPrice.innerText = price;
     const itemImg = document.createElement("img");
     itemImg.src = image;
     menuItem1.appendChild(title);
     menuItem1.appendChild(itemDescription);
+    menuItem1.appendChild(itemPrice);
     menuItem1.appendChild(itemImg);
     return menuItem1;
 }
@@ -23,8 +26,8 @@ export const loadMenu = () => {
         contentDiv.removeChild(contentDiv.firstChild);
         const subContentDiv = document.createElement("div");
         subContentDiv.classList.add("sub-content");
-        const item1 = createMenuItem("Pancakes", "Fluffy, crisp, and delightful!", pancakes);
-        const item2 = createMenuItem("Bacon", "Meaty, greasy, will clog your arteries!", bacon);
+        const item1 = createMenuItem("Pancakes", "Fluffy, crisp, and delightful!", pancakes, "$9.99");
+        const item2 = createMenuItem("Bacon", "Meaty, greasy, will clog your arteries!", bacon, "$4.99");
         subContentDiv.appendChild(item1);
         subContentDiv.appendChild(item2);
         contentDiv.appendChild(subContentDiv);
